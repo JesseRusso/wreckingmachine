011fe761f0500e4011c6e2f94336fef3e308c677
Jesse Russo <jesserusso90@gmail.com>
2022-09-20 23:21:38 -0700

    evaluates pairs w/ history display

--- script.js
+++ script.js
@@ -1,58 +1,85 @@
-let displayValue = null;
-let num1 = null;
-let result = null;
+let displayValue = "";
 let prevAns = null;
 let operator = null;
+let numArray = [];
 const operators = document.querySelectorAll('.operator');
 const numbers = document.querySelectorAll(".number");
 const display = document.querySelector('.display');
+const history = document.querySelector('.history');
 document.getElementById('clear-button').addEventListener('click', (e) => { clear() });
 
 document.getElementById('equals').addEventListener('click', (e) => {
-    result = operate(operator, num1, displayValue);
-    display.innerText = result;
-    prevAns = result;
+    if (displayValue != null) {
+        operate(operator, numArray[0], displayValue);
+    }
 });
-
-display.innerText = Number(displayValue);
+display.innerText = displayValue;
 numbers.forEach(element => {
     element.addEventListener("click", (e) => {
         if (displayValue == null) {
-            displayValue = Number(element.innerText);
+            displayValue = element.innerText;
             display.innerText = displayValue;
         }
-        else if (String(displayValue).length < 11) {
+        else if (displayValue.length < 11) {
             displayValue += element.innerText;
             display.innerText = displayValue;
-            displayValue = Number(displayValue);
+            displayValue = displayValue;
         }
     })
 });
 operators.forEach(element => {
     element.addEventListener('click', (e) => {
-        if(prevAns != null){
-            num1 = prevAns;
-            displayValue = null;
-            operator = element.innerText;
-            display.innerText += element.innerText;
-            return;
-        }
-        num1 = Number(displayValue);
-        displayValue = null;
-        operator = element.innerText;
-        display.innerText += element.innerText;
+        addOp(element.innerText);
     })
 });
 function clear() {
     displayValue = null;
     operator = null;
     prevAns = null;
-    num1 = null;
+    history.innerText = "";
+    numArray = [];
     display.innerText = displayValue;
 }
+function addOp(op) {
+    switch (numArray.length) {
+        case 0:
+            numArray.push(displayValue);
+            operator = op;
+            displayValue = null;
+            display.innerText = displayValue;
+            history.innerText = `${numArray[0]} ${op}`;
+            break;
+        case 1:
+            numArray.push(displayValue);
+            operate(operator, numArray[0], numArray[1]);
+            history.innerText = `${prevAns} ${op}`;
+            numArray = [];
+            numArray[0] = prevAns;
+            operator = op;
+            displayValue = null;
+            break;
+        case 2:
+            operate(operator, numArray[0], numArray[1]);
+            numArray = [];
+            numArray[0] = prevAns;
+            operator = op;
+            displayValue = null;
+            break;
+        default: console.log('oooops');
+            break;
+    }
+}
 function operate(op, a, b) {
-    let result = choose[op](a, b);
-    return Number(result);
+    numa = Number(a);
+    numb = Number(b);
+    history.innerText = `${numa} ${op} ${numb} = `;
+    if (op == '/' && numb == 0) {
+        display.innerText = ':|';
+        return;
+    }
+    prevAns = choose[op](numa, numb);
+    display.innerText = prevAns;
+    return prevAns;
 }
 const choose = {
     '+': (a, b) => { return a + b; },
